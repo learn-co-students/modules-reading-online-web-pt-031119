acafb76a719ffc74e16919ab24e7d7d4dc0773e3
Daniel Lima <daniel.echuaca.lima@gmail.com>
2019-04-12 19:51:02 +0000

    Done.

--- lib/kid.rb
+++ lib/kid.rb
@@ -4,11 +4,8 @@ require_relative './fancy_dance.rb'
 
 class Kid
   
-  extend
-  FancyDance::ClassMethods 
-  
-  include
-  FancyDance::InstanceMethods
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
  
   attr_accessor :name
  
